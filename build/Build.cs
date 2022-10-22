7f1e13da05efd40b512b4aab6442aadddfc648cc
MarkZither <mark.burton@zither-it.co.uk>
2022-10-23 00:07:08 +0200

    Add test target to build

--- build/Build.cs
+++ build/Build.cs
@@ -1,26 +1,34 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
+using Nuke.Common.CI.AzurePipelines;
 using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.ReportGenerator;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Coverlet;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.CompressionTasks;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.ReportGenerator.ReportGeneratorTasks;
 
 [ShutdownDotNetAfterServerBuild]
 [GitHubActions(
     "continuous",
     GitHubActionsImage.UbuntuLatest,
     On = new[] { GitHubActionsTrigger.Push },
-    InvokedTargets = new[] { nameof(Compile) })]
+    InvokedTargets = new[] { nameof(Test) })]
 [GitHubActions(
     "sonar",
     GitHubActionsImage.UbuntuLatest,
@@ -35,7 +43,7 @@ public partial class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.Test);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -73,4 +81,64 @@ public partial class Build : NukeBuild
                 .EnableNoRestore());
         });
 
+    [Partition(2)] readonly Partition TestPartition;
+    AbsolutePath TestResultDirectory => OutputDirectory / "test-results";
+    IEnumerable<Project> TestProjects => TestPartition.GetCurrent(Solution.GetProjects("*.Tests"));
+    Target Test => _ => _
+            .DependsOn(Compile)
+            .Produces(TestResultDirectory / "*.trx")
+            .Produces(TestResultDirectory / "*.xml")
+            .Partition(2)
+            .Executes(() =>
+            {
+                DotNetTest(_ => _
+                    .SetConfiguration(Configuration)
+                    //.SetFilter("Category=Unit")
+                    .SetNoBuild(InvokedTargets.Contains(Compile))
+                    .ResetVerbosity()
+                    .SetResultsDirectory(TestResultDirectory)
+                    .When(InvokedTargets.Contains(Coverage) || IsServerBuild, _ => _
+                        .EnableCollectCoverage()
+                        .SetCoverletOutputFormat(CoverletOutputFormat.cobertura)
+                        .SetExcludeByFile("*.Generated.cs")
+                        .When(IsServerBuild, _ => _
+                            .EnableUseSourceLink()))
+                    .CombineWith(TestProjects, (_, v) => _
+                        .SetProjectFile(v)
+                        .SetLoggers($"trx;LogFileName={v.Name}.trx")
+                        .SetCoverletOutput(TestResultDirectory / $"{v.Name}.xml")));
+
+                TestResultDirectory.GlobFiles("*.trx").ForEach(x =>
+                    AzurePipelines.Instance?.PublishTestResults(
+                        type: AzurePipelinesTestResultsType.VSTest,
+                        title: $"{Path.GetFileNameWithoutExtension(x)} ({AzurePipelines.Instance?.StageDisplayName})",
+                        files: new string[] { x }));
+            });
+    string CoverageReportDirectory => OutputDirectory / "coverage-report";
+    string CoverageReportArchive => OutputDirectory / "coverage-report.zip";
+
+    Target Coverage => _ => _
+        .DependsOn(Test)
+        //.TryAfter(Test)
+        .Consumes(Test)
+        .Produces(CoverageReportArchive)
+        .Executes(() =>
+        {
+            ReportGenerator(_ => _
+                .SetReports(TestResultDirectory / "*.xml")
+                .SetReportTypes(ReportTypes.HtmlInline)
+                .SetTargetDirectory(CoverageReportDirectory)
+                .SetFramework("net5.0"));
+
+            TestResultDirectory.GlobFiles("*.xml").ForEach(x =>
+                AzurePipelines.Instance?.PublishCodeCoverage(
+                    AzurePipelinesCodeCoverageToolType.Cobertura,
+                    x,
+                    CoverageReportDirectory));
+
+            CompressZip(
+                directory: CoverageReportDirectory,
+                archiveFile: CoverageReportArchive,
+                fileMode: FileMode.Create);
+        });
 }
